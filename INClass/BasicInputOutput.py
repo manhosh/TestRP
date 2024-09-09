16ef43756ff5db3e9a764303ec498fde10985cdd
manhosh <w0481635@nscc.ca>
2024-09-09 11:50:58 -0300

    Another versian of the code

---
+++ INClass/BasicInputOutput.py
@@ -0,0 +1,32 @@
+#Student Name:  Mansoor  
+#Program Title:Prog 1015  
+#Description:  Practice  
+
+def main(): #<-- Don't change this line!
+    #Write your code below. It must be indented!
+
+    # input 
+    '''
+    it is just a comment
+    
+    '''
+
+    firstName = "Mansoor"
+    lastName = " LAla"
+
+    # Process
+
+    fullName = firstName + lastName
+
+    # OutPut
+    print('This is the first line')
+    print(" I am printing my code")
+    print("My full name is ", fullName)
+
+
+
+    #Your code ends on the line above
+
+#Do not change any of the code below!
+
+main()
---
+++ INClass/ProgramTemplate.py
@@ -0,0 +1,15 @@
+#Student Name:    
+#Program Title:  
+#Description:    
+
+def main(): #<-- Don't change this line!
+    #Write your code below. It must be indented!
+
+
+
+
+    #Your code ends on the line above
+
+#Do not change any of the code below!
+if __name__ == "__main__":
+    main()
